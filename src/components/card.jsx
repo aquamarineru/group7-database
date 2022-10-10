ab5496db0953ffd1f35be4d0d0ed5ad5665772d3
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-10-10 16:05:52 +0200

    some  changes

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Card(carddata) {
 	return (
 			<div className="userCards">
+			 {/* <Link to={`/person/${carddata.id}`}> */}
 				<p>{carddata.id}</p>
 				<img src={carddata.avatar} alt="" />
 				<p>{carddata.name}</p>
 				<p>{carddata.Country}</p>
+
 			</div>
 
 	);
